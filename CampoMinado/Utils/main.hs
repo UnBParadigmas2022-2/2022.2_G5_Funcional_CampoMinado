44e67ffb3a07b6e5407c99e624c28af57bd080a1
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-11-23 20:29:36 -0300

    adicionando menu base do jogo e main

--- CampoMinado/Utils/main.hs
+++ CampoMinado/Utils/main.hs
@@ -1,2 +1,8 @@
+import Mensagens
+
 inverte:: Int -> Int -> Matriz -> Matriz-> Matriz
 inverte num1 num2 matriz anterior = reverse(verificaSoma num1 num2 matriz anterior)
+
+main :: IO()
+main = do
+    Mensagens.messageMenu
