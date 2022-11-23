34d4acb81ee4176086559e2dfc0c58041ba94c06
João Pedro Chaves <joaopedroaschaves@gmail.com>
2022-11-23 19:59:20 -0300

    Criação da função foiRevelado

--- CampoMinado/Utils/Reveladoras.hs
+++ CampoMinado/Utils/Reveladoras.hs
@@ -1,6 +1,7 @@
 module Reveladoras
 (
-contaNaoRevelados
+contaNaoRevelados,
+foiRevelado,
 )where
 
 type Coordenadas = (Int, Int)
@@ -8,6 +9,13 @@ type Valor = Int
 type Elem = (Coordenadas,Valor)
 type Matriz = [Elem]
 
+-- Dicionário das siglas
+-- cm: cauda da matriz
+
+
 contaNaoRevelados :: Int -> Matriz -> Int
 contaNaoRevelados num [] = num
-contaNaoRevelados num (((x, y), v) : mtz) = if (v == -2) then (contaNaoRevelados (num+1) mtz) else (contaNaoRevelados (num) mtz)
+contaNaoRevelados num (((x, y), v) : matriz) = if (v == (-2)) then (contaNaoRevelados (num+1) matriz) else (contaNaoRevelados (num) matriz)
+
+foiRevelado :: Int -> Int -> Matriz -> Bool
+foiRevelado x y (((a, b), v) : cm) = if (a == x && b == y) then (v /= (-2)) else (foiRevelado x y cm)
