0c45f489eeda40d7c3e7e096fcf15aa0ddbaeb93
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-11-23 22:40:19 -0300

    adicionando dificuldade ao jogo

--- CampoMinado/Utils/main.hs
+++ CampoMinado/Utils/main.hs
@@ -1,10 +1,24 @@
 import Mensagens
 
--- inverte:: Int -> Int -> Matriz -> Matriz-> Matriz
--- inverte num1 num2 matriz anterior = reverse(verificaSoma num1 num2 matriz anterior)
+inicia_jogo :: IO()
+inicia_jogo = do
+
+    putStrLn "Escolha seu nível :" 
+    putStrLn "1 - Fácil\n2 - Médio\n3 - Difícil\n"
+    putStr "Sua opção:"
+
+    entrada <- getLine
+    putStrLn "\n"
+    let entradaLinha = verificaDificuldade entrada
+    let array = words entradaLinha
+    let linhas = read (array !! 0) :: Int
+    let colunas = read (array !! 1) :: Int
+    let bombas = read (array !! 2) :: Int
 
 main :: IO()
 main = do
     Mensagens.messageMenu
     Mensagens.messageVitoria
     Mensagens.messageDerrota
+
+    inicia_jogo
