3cea3715192ca951e9cbdf82f5cd7e809b54db78
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-11-23 21:56:03 -0300

    adicionando mensagens do game

--- CampoMinado/Utils/main.hs
+++ CampoMinado/Utils/main.hs
@@ -1,8 +1,10 @@
 import Mensagens
 
-inverte:: Int -> Int -> Matriz -> Matriz-> Matriz
-inverte num1 num2 matriz anterior = reverse(verificaSoma num1 num2 matriz anterior)
+-- inverte:: Int -> Int -> Matriz -> Matriz-> Matriz
+-- inverte num1 num2 matriz anterior = reverse(verificaSoma num1 num2 matriz anterior)
 
 main :: IO()
 main = do
     Mensagens.messageMenu
+    Mensagens.messageVitoria
+    Mensagens.messageDerrota
